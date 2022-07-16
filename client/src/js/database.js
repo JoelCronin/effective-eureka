0f3220b4b76762d3c0fa5d993e49a227fcbc6e6b
joel cronin <you@example.com>
2022-07-16 13:00:59 -0400

    comments cleaned up

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -12,8 +12,7 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
-// export const putDb = async (content) => console.error('putDb not implemented');
+
 
 export const putDb = async (content) => {
   console.log('PUT to the database');
@@ -25,8 +24,6 @@ export const putDb = async (content) => {
   console.log('🚀 - data saved to the database', result);
 };
 
-// TODO: Add logic for a method that gets all the content from the database
-// export const getDb = async () => console.error('getDb not implemented');
 
 export const getDb = async () => {
   console.log('GET all from the database');
